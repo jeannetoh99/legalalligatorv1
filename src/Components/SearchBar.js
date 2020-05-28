34e5c2ca0ca3e200f72a2d6e4d60ab97b4e2a331
jeannetoh <jeannetoh99@gmail.com>
2020-05-29 03:38:34 +0800

    minor edits

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,29 +4,28 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 'Please describe your case.',
-            wordCount:'4'
+            value: '',
+            wordCount:'0'
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        const wordCount =
-      event.target.value === "" ? 0 : event.target.value.split(" ").length;
-      this.setState({ value: event.target.value, wordCount: wordCount });
+    handleChange(e) {
+        const wordCount = e.target.value === "" ? 0 : e.target.value.split(" ").length;
+        this.setState({ value: e.target.value, wordCount: wordCount });
     }
 
-    handleSubmit(event) {
+    handleSubmit(e) {
         alert('A case was submitted: ' + this.state.value);
-        event.preventDefault();
+        e.preventDefault();
     }
 
     render() {
         return (
-            <div className="flex justify-center" >
-                <div className="rounded-lg box-border h-96 w-236 p-4 border-4 border-orange-400 bg-orange-200">
+            <div className="flex justify-center mx-6 mb-40" >
+                <div className="rounded-lg h-96 w-236 py-4 px-12 bg-red-400">
                     {/* <div className="flex justify-center">
                         <div className="self-end flex-1 rounded-lg h-48 w-196 bg-white"> */}
                     <form className="flex flex-col" onSubmit={this.handleSubmit}>
@@ -35,15 +34,21 @@ class SearchBar extends Component {
                             <label className="font-normal text-xl">
                                 Briefly describe what happened and what you are seeking to do. Our machines
                                 will read and scan through the database for relevant topics to assist you.</label>
-                            <textarea className="bg-gray-100 rounded-lg p-2 h-48 text-lg" value={this.state.value} onChange={this.handleChange} />
+                            <textarea 
+                                className="bg-gray-100 rounded-lg p-2 h-40 text-lg mt-4" 
+                                placeholder="Please describe your case."
+                                onChange={this.handleChange} 
+                            />
                         </label>
                         <div className="flex flex-row justify-between">
-                    <div className="p-2 text-sm">wordcount: {this.state.wordCount}</div>
-                        <div className="p-2 self-end">
-                            <input className="bg-green-400 p-2 rounded-lg" type="submit" value="Submit" />
-                        </div>
-                        </div>
-                    </form>
+                            <div className="p-2 text-sm">Word Count: {this.state.wordCount}/50</div>
+                                <input 
+                                    className="mt-4 h-10 w-24 bg-red-500 self-end rounded-lg text-white shadow-xl cursor-pointer hover:bg-red-300" 
+                                    type="submit" 
+                                    value="Submit" 
+                                />
+                            </div>
+                        </form>
                     {/* </div>
                     </div> */}
                 </div>
