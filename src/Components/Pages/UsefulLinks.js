faca163a15de3daafd1a96ab4889f64faa5e5485
kerwin97 <kerwinlim97@hotmail.com>
2020-05-29 02:31:30 +0800

    added simple background for pages

--- src/Components/Pages/UsefulLinks.js
+++ src/Components/Pages/UsefulLinks.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const UsefulLinks = () => {
-    return (
-      <div>
-          Useful Links here! :)
+  return (
+    <div>
+      <div className="bg-opacity-50 bg-cover bg-yellow-200 bg-fixed h-64 flex items-center" >
+        <div className="text-center karla text-4xl font-semibold flex-1 ">
+          Useful Links</div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default UsefulLinks;
